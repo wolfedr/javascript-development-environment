40eadd8e714c26dd518fcd555159e6b447e36778
PCOWNER <PCOWNER>
2017-04-08 09:59:26 -0400

    updateing after lint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import path from 'path';
-import open from'open';
+import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
-
+/* eslint-disable no-console */
 const port = 3000;
 const app = express();
-const compiler =  webpack(config);
+const compiler = webpack(config);
 
-app.use(require('webpack-dev-middeware')(compiler {
-  inInfo: true,
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
   publicPath: config.output.publicPath
 }));
 
@@ -19,7 +19,6 @@ app.use(require('webpack-dev-middeware')(compiler {
 app.get('/',function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
-
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import numeral from 'numeral';
+import './index.css';
+
+
 
 const courseValue = numeral(1000).format('$0,0.00');
-console.log('I would papy ${courseValue] for this awesome course!');
+console.log('I would pay ' + courseValue + ' for this awesome course!');//eslint-disable-line no-console
