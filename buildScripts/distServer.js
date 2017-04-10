fe6faec9b26829c421d752cacfb5d9202b2f3059
PCOWNER <PCOWNER>
2017-04-10 19:46:17 -0400

    Committing changes up to automate prod dev

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,21 +1,18 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-//following imports are needed for development.
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
+
+
 /* eslint-disable no-console */
 const port = 3000;
 const app = express();
-const compiler = webpack(config);    //not needed on porduction server
 
-app.use(require('webpack-dev-middleware')(compiler, {  //this call not needed on prod.l
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-
+app.use(compression);
+app.use(express.static('dist'));
+ 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.hrml'));
 });
 
 app.get('/users', function(req, res) {
