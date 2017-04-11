40077172d415e34e33e45bf14ebf98ddd3ae4c1a
PCOWNER <PCOWNER>
2017-04-11 09:54:47 -0400

    Changes bundling cache busting minify split bundle

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -3,16 +3,16 @@ import path from 'path';
 import open from 'open';
 import compression from 'compression';
 
-
 /* eslint-disable no-console */
+
 const port = 3000;
 const app = express();
 
-app.use(compression);
+app.use(compression());
 app.use(express.static('dist'));
- 
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../dist/index.hrml'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.get('/users', function(req, res) {
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,52 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //generate an external css file with a hash in the name
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    //Hash the files using MD5 so that their names change when the content  changes This triggers cache busting
+    new WebpackMd5Hash(),
+
+    // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    //Create HTML file that includes reference to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minfyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+    inject: true
+    }),
     //Eliminate Duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
     //minify JS
@@ -23,7 +55,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
